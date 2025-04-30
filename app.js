9f1a55d36ffe3c43321e9be3ca7c28bd894f68f4
Omoyele <ewaregbemi@gmail.com>
2025-04-30 16:48:31 +0100

    update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello, Yele!');
-});
+  res.send('Hello, Yele!')
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
